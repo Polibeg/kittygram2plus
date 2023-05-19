928e6d62d88c7d5f92bd4b5e6b7c529731118566
Полина Бегалиева <POLINA.BUTTERFLY@MAIL.RU>
2023-05-19 12:57:22 +0300

    Пагинация и авторизация ужились

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -127,6 +127,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.ScopedRateThrottle',
     ],
